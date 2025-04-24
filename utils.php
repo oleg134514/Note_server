c71d019f5bbd7768ceca8bf3775c0e0227c4a4e9
oleg134514 <168273249+oleg134514@users.noreply.github.com>
2025-04-24 18:14:28 +0500

    Delete utils.php

--- utils.php
+++
@@ -1,8 +0,0 @@
-<?php
-function generate_csrf_token() {
-    if (!isset($_SESSION['csrf_token'])) {
-        $_SESSION['csrf_token'] = bin2hex(random_bytes(32));
-    }
-    return $_SESSION['csrf_token'];
-}
-?>
